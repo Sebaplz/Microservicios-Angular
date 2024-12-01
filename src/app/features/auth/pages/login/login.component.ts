11a548806ad7b783986df9d6d6c0eed4b5c81107
Sebaplz <sebastian.h.neira@outlook.com>
2024-12-01 13:45:18 -0300

    Se agregan estilos y validaciones al login.component.ts

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,27 +1,53 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {DividerModule} from 'primeng/divider';
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import {CommonModule} from '@angular/common';
+import {MessagesModule} from 'primeng/messages';
+import {FloatLabelModule} from 'primeng/floatlabel';
+import {MessageModule} from 'primeng/message';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, DividerModule, ButtonModule, InputTextModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, DividerModule, ButtonModule, InputTextModule, ReactiveFormsModule, RouterLink, MessagesModule, FloatLabelModule, FormsModule, MessageModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   private formBuilder = inject(FormBuilder);
-  errorMessage: string = '';
 
   /** Login form group */
   loginForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]], // Coloca los validadores síncronos en un array
-    password: ['', [Validators.required, Validators.minLength(6)]], // Coloca los validadores síncronos en un array
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
   });
+
+  /**
+   * Returns error message for a specific field
+   * @param field Field name
+   * @returns Error message string
+   */
+  getErrorMessage(field: string): string {
+    const control = this.loginForm.get(field);
+
+    if (control?.hasError('required')) {
+      return `${field} is required.`;
+    }
+
+    if (field === 'email' && control?.hasError('email')) {
+      return `Invalid email format.`;
+    }
+
+    if (field === 'password' && control?.hasError('minlength')) {
+      const requiredLength = control.getError('minlength')?.requiredLength;
+      return `Password must be at least ${requiredLength} characters long.`;
+    }
+
+    return '';
+  }
 
   /**
    * Handles the login form submission
